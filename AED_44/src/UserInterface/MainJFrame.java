7c6596a420aeb935f93dee895f813097cdafe2b4
Anouksha Sreemurthy <anoukshasreemurthy@Anoukshas-MacBook-Pro.local>
2022-12-11 16:53:54 -0500

    UI2

--- AED_44/src/UserInterface/MainJFrame.java
+++ AED_44/src/UserInterface/MainJFrame.java
@@ -30,7 +30,6 @@ public class MainJFrame extends javax.swing.JFrame {
     public MainJFrame() {
         system = dB4OUtil.retrieveSystem();
         initComponents();
-        this.setSize(1680, 1050);
     }
 
     /**
